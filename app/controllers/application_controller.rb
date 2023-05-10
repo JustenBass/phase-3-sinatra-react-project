56b1cf28b39478dd91c044175c64de19b5eb68e6
Justin Bass <justinbass6036@gmail.com>
2023-05-10 14:37:23 -0400

    commit message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,5 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
-  # Add your routes here
-  get "/actors" do
-    actors = Actor.all
-end
-
-get "/actors/movies/:id" do
-find_actor = Actor.actor_list_of_movies(params[:id])
-find_actor.to_json
-end
-
-get "/actors/count" do
-  actor_count = Actor.actor_count
-  phrase = "There are #{actor_count} actors!"
-  phrase.to_json
-end
-
-get "/movies" do
-  movies = Movie.find_movie_by_year
-  movies.to_json
-end
-
-
 
 end
