60f6cea83999f1059f4d5e5f32c9fe5ce8ce4746
Justin Bass <justinbass6036@gmail.com>
2023-05-31 18:31:21 -0400

    commit message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,62 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
+  get '/people' do
+    Person.all.to_json(include: :errands)
+  end
+
+
+
+  post '/people' do
+    new_person = Person.create(params)
+    new_person.to_json(include: :errands)
+  end
+
+  post '/people/:id/errands' do
+    person = Person.find(params[:id])
+    newErrand = person.errands.create(
+      errand: params[:errand],
+      date: params[:date],
+      time: params[:time],
+      location: params[:location],
+      address: params[:address],
+      commute: params[:commute],
+      )
+      newErrand.to_json
+  end
+
+  # post '/errands' do
+  #   person = Person.find_by(id: params[:person_id])
+  #   errand = person.errands.create(
+  #     errand: params[:errand],
+  #     date: params[:date],
+  #     time: params[:time],
+  #     location: params[:location],
+  #     address: params[:address],
+  #     commute: params[:commute],
+  #     )
+  #   errand.to_json
+  # end
+
+
+  patch '/errands/:id' do
+    update_errands = Errand.find(params[:id])
+    update_errands.update(
+      errand: params[:errand],
+      date: params[:date],
+      time: params[:time],
+      location: params[:location],
+      address: params[:address],
+      commute: params[:commute]
+    )
+    # binding.pry refactor look through person errands
+    update_errands.to_json
+  end
+
+  delete '/errands/:id' do
+    delete_errands = Errand.find(params[:id])
+    delete_errands.destroy
+    #  delete off the persons table
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,4 @@
 puts "🌱 Seeding data..."
 
-    movies = [
-    "The Bullet Train",
-    "Once Upon A Time In... Hollywood",
-    "MoneyBall",
-    "Fight Club",
-    "Troy",
-    "Interview With The Vampire",
-    "Tomb Raider"
-]
-
-    brad_pitt = Actor.create(name: "Brad Pitt", golden_globe_awards: 2 )
-    angelina_jolie = Actor.create(name: "Angelina Jolie", golden_globe_awards: 3)
-
-    Movie.create(name: movies[0], actor_id: brad_pitt.id)
-    Movie.create(name: movies[1], actor_id: brad_pitt.id)
-    Movie.create(name: movies[2], actor_id: brad_pitt.id)
-    Movie.create(name: movies[3], actor_id: brad_pitt.id)
-    Movie.create(name: movies[4], actor_id: brad_pitt.id)
-    Movie.create(name: movies[5], actor_id: brad_pitt.id)
-    Movie.create(name: movies[6], actor_id: angelina_jolie.id)
-
 
 puts "✅ Done seeding!"
