c1faf9870721060c0b355203e366f624daf37dd1
Justin Bass <justinbass6036@gmail.com>
2023-05-31 23:32:14 -0400

    commit message

--- db/migrate/20230601033052_add_column_to_errands_table.rb
+++ db/migrate/20230601033052_add_column_to_errands_table.rb
@@ -1,4 +1,5 @@
 class AddColumnToErrandsTable < ActiveRecord::Migration[6.1]
   def change
+    add_column :errands, :am_pm, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_16_064320) do
+ActiveRecord::Schema.define(version: 2023_06_01_033052) do
 
   create_table "errands", force: :cascade do |t|
     t.string "errand"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2023_05_16_064320) do
     t.string "address"
     t.integer "commute"
     t.integer "person_id"
+    t.string "am_pm"
   end
 
   create_table "people", force: :cascade do |t|
