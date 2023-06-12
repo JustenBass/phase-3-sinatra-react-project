61d82aa010cb72a0ccf5b049de6b02087a35fd58
Justin Bass <justinbass6036@gmail.com>
2023-06-12 19:45:48 -0400

    Initial commit of all files to the repository

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,47 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
 
+  get '/people' do
+    Person.all.to_json(include: :errands)
+  end
 
+  post '/people' do
+    new_person = Person.create(params)
+    new_person.to_json(include: :errands)
+  end
+
+  post '/people/:person_id/errands' do
+    person = Person.find(params[:person_id])
+    newErrand = person.errands.create(
+      errand: params[:errand],
+      date: params[:date],
+      time: params[:time],
+      am_pm: params[:am_pm],
+      location: params[:location],
+      address: params[:address],
+      )
+      newErrand.to_json
+  end
+
+  patch '/people/:person_id/errands/:errand_id' do
+    editErrand = Errand.find(params[:errand_id])
+
+    editErrand.update(
+      errand: params[:errand],
+      date: params[:date],
+      time: params[:time],
+      am_pm: params[:am_pm],
+      location: params[:location],
+      address: params[:address],
+    )
+    editErrand.to_json
+  end
+
+
+  delete '/people/:person_id/errands/:errand_id' do
+    delete_errand = Errand.find(params[:errand_id])
+    delete_errand.destroy
+
+  end
 
 end
